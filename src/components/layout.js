7aadecc91f8729d7fa2ba59ad12e6997033a19cf
Parag Kushwah <parag.arishi@gmail.com>
2024-09-05 17:12:14 +0530

    Improved Structure

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,18 @@
 import React from "react";
-import Header from './header'
+import Header from "./header/header";
 import { container, heading, mainContent } from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <div className={container}>
+    <>
       <Header />
-      <main className={mainContent}>
-        <h1 className={heading}>{pageTitle}</h1>
-        {children}
-      </main>
-    </div>
+      <div className={container}>
+        <main className={mainContent}>
+          <h1 className={heading}>{pageTitle}</h1>
+          {children}
+        </main>
+      </div>
+    </>
   );
 };
 
